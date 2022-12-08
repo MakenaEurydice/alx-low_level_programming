dfea0ae7358b6060276ab6fee70b68ca710250de
MakenaEurydice <t0eurydice@gmail.com>
2022-12-08 02:15:34 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
+ * main -main method
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: always 0 (successful execution)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

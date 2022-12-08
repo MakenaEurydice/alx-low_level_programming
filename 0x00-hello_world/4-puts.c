2ec4f2b88deb91bc1af4711706d1479c396373c0
MakenaEurydice <t0eurydice@gmail.com>
2022-12-08 01:40:25 -0800

    correctio

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,15 @@
 int main(void)
 
 {
+	/**
+	 *
+	 *  * main - mainnn method
+	 *
+	 *   *
+	 *
+	 *    * Return: 0 for successful completion
+	 *
+	 *     */
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

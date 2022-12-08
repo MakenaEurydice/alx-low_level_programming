ed91781d960550e82dd5f27aa1279a31e6969457
MakenaEurydice <t0eurydice@gmail.com>
2022-12-08 02:36:05 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n"
-	write(2,  err, 59);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return(1);
 }

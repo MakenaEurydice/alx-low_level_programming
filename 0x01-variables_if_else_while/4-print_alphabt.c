cde90b155ca526ae8d79b50fa273300efc2c3c87
MakenaEurydice <t0eurydice@gmail.com>
2022-12-09 10:24:04 -0800

    q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the alphabet on a line omitting q and e
+ * using putchar only twice
+ * Return: 0
+ */
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+if (alpha == 'e' || alpha == 'q')
+alpha++;
+}
+putchar('\n');
+return (0);
+
+}

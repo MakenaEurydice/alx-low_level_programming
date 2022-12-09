1846764402151ac9c026d8888906c76fd58e1ae0
MakenaEurydice <t0eurydice@gmail.com>
2022-12-09 10:19:22 -0800

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,22 +8,22 @@
  */
 int main(void)
 {
-	char alpha = 'a';
+char alpha = 'a';
 
-	while (alpha <= 'z')
-	{
-		putchar(alpha);
-		alpha++;
-	}
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
 
-	alpha = 'A';
+alpha = 'A';
 
-	while (alpha <= 'Z')
-	{
-		putchar(alpha);
-		alpha++;
-	}
-	putchar('\n');
+while (alpha <= 'Z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
 
-	return (0)
+return (0)
 }

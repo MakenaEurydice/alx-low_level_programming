5c74da458cc16e188af3e6a1a864de6b47f7be76
MakenaEurydice <t0eurydice@gmail.com>
2022-12-14 03:55:45 -0800

    addition of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of all multiples of 3 or 5 up to 1024
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+		{
+			sum += i;
+		}
+	}
+
+		printf("%d\n", sum);
+
+		return (0);
+}	

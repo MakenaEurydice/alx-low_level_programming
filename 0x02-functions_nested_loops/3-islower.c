57637cb0a915d396455d8832ee0058ee584b0353
MakenaEurydice <t0eurydice@gmail.com>
2022-12-14 01:28:11 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - Entry point
+ * _islower - Checks for lowercase character
  * Description - Checks for lowercase character
+ * @c: The character to be checked
  *
  * Return 1 for lowercase character or 0 for anything else
  */

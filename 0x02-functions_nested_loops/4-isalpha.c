5685b7d6aaf7177e69ef0f99d7f378f5a2da0046
MakenaEurydice <t0eurydice@gmail.com>
2022-12-14 01:35:06 -0800

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,8 @@
 /**
  * _isalpha: checks for alphabetic character
  * @c: The character to be checked
- *
+ * Description:  checks for alphabetic character
+ * 
  * Return: 1 if c is a letter, lowercase or uppercase and 0 otherwise
  */
 int _isalpha(int c)

6a495c08c3ff10d569ab99fa5c2ca40499c01e5f
MakenaEurydice <t0eurydice@gmail.com>
2022-12-14 02:05:48 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,14 @@ int print_last_digit(int r)
 	int n;
 
 	if (r < 0)
-		 n = -1 * (r % 10);
+	{
+		n = -1 * (r % 10);
+	}
 	else
+	{
 		n = r % 10;
-
+	}
 	_putchar((n % 10) + '0');
+
 	return (n % 10);
 }

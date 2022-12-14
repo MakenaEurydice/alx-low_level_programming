51db374e0c51b1cda1779ae885dc07e7256ea776
MakenaEurydice <t0eurydice@gmail.com>
2022-12-14 02:58:31 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main: prints _putchar, followed by a new line.
+ * main - prints _putchar, followed by a new line.
  * Description: a program that prints _putchar, followed by a new line
  *
  * Return: Always 0

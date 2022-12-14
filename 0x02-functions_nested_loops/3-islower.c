ec0346d4d49549eeb46bea9fa9d1a7361d142aa1
MakenaEurydice <t0eurydice@gmail.com>
2022-12-14 01:29:25 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * Description - Checks for lowercase character
  * @c: The character to be checked
  *
- * Return 1 for lowercase character or 0 for anything else
+ * Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
 {

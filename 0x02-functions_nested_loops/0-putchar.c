ca7ad63190eb2a5dbe59ca2c6e32d13126f67e3f
MakenaEurydice <t0eurydice@gmail.com>
2022-12-14 02:56:03 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,20 @@
 #include "main.h"
 
 /**
- * main Entry point
+ * main: Entry point
+ * Description: a program that prints _putchar, followed by a new line
  *
  * Return: Always 0
  */
 int main(void)
 {
-//	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
 	_putchar('\n');
 
 	return (0);

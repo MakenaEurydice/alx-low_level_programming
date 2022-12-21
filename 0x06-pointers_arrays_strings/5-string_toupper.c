988d8f15f200bdc77eb1823ff1cdb7e44748ffd5
MakenaEurydice <t0eurydice@gmail.com>
2022-12-21 10:25:00 -0800

    string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,7 +6,7 @@
  * @s: an input string
  * Return: char pointer to converted string
  */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
 	char *start = s;
 
@@ -17,4 +17,4 @@ char *string_toupper(char *)
 		s++;
 	}
 	return (start);
-}
+}				

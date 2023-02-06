b450a1db9b42f6dd01cfd2bed034bd9638eba3b4
Makena <43780622+MakenaEurydice@users.noreply.github.com>
2023-02-06 19:47:28 +0300

    correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
+    	unsigned int a = 0, max = 32768; /* 1000 0000 0000 0000 */
 
 	if (n == 0)
-	{
-		_putchar('0');
+    {
+        _putchar('0');
 		return;
 	}
 	while (max)
 	{
-		if (flag == 1 && (n & max) == 0)
+    	if (a == 1 && (n & max) == 0)
 			_putchar('0');
-		else if ((n & max) != 0)
+            	else if ((n & max) != 0)
 		{
 			_putchar('1');
-			flag = 1;
+			a = 1;
 		}
 		max >>= 1;
 	}
